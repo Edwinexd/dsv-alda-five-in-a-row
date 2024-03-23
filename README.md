03851780e8802fac091a949e739d60cf0449ad83
Edwin <60476129+Edwinexd@users.noreply.github.com>
2024-03-23 18:26:46 +0000

    Update README and License under GPL v3.0

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# dsv-alda-five-in-a-row
+# Five in a Row (Gomoku) with Minmax Algorithm
+A Java implementation of the game "Five in a Row" (Gomoku) using the minmax algorithm with alpha-beta pruning.
+
+Written as a project for the course "Algoritmer och datastrukturer" at Stockholm University.
+
+## License
+This project is licensed under the GNU General Public License v3.0 - see the [LICENSE](LICENSE.md) file for details.
--- src/main/java/com/edsv/Segment.java
+++ src/main/java/com/edsv/Segment.java
@@ -1,3 +1,20 @@
+/* 
+dsv-alda-five-in-a-row: A Java implementation of the game "Five in a row" (Gomoku) with an AI using the Minimax algorithm and alpha-beta pruning.
+Copyright (C) 2024 Edwin Sundberg
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 package com.edsv;
 
 public record Segment(int length, int openEnds) implements Comparable<Segment> {
